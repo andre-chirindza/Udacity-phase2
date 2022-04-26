3cf39a4802f01145d2e4ed6a09340e94ca73a120
andre-chirindza <andrechirindza@gmail.com>
2022-04-26 08:11:28 +0200

    Moves work

---
+++ projects/arcadeGame/README.md
@@ -0,0 +1,13 @@
+# Classic Arcade Game Clone Project
+
+## Table of Contents
+
+- [Run](#run)
+- [Instructions](#instructions)
+
+## Run
+
+
+## Instructions
+
+
---
+++ projects/arcadeGame/js/app.js
@@ -0,0 +1,180 @@
+let moves = document.getElementById("moves");
+let timer = document.getElementById("timer");
+let life = document.getElementById("life");
+let scores = document.getElementById("scores");
+let moveValue = 85;
+
+let updateMoves = () => {
+    let value = moves.textContent;
+    moves.textContent = parseInt(value) + 1;
+};
+let updateScores = () => {
+    let value = scores.textContent;
+    scores.textContent = parseInt(value) + 1;
+};
+
+let updateLifes = lifes => {
+    life.textContent = lifes;
+};
+
+// Enemies our player must avoid
+var Enemy = function (sprite, x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+    this.speed = Math.random() * 100 + 120;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if (this.x <= ctx.canvas.width) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = -120;
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+let Player = function (sprite) {
+    this.sprite = sprite;
+    this.life = 3;
+    this.x = 200;
+    this.y = 420;
+};
+
+Player.prototype = {
+    constructor: Player,
+    render: function () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        updateLifes(this.life)
+    },
+    update: function () {},
+    handleInput: function (allowedKeys) {
+        switch (allowedKeys) {
+            case "left":
+                if (this.x >= 100) {
+                    updateMoves();
+                    this.x -= 100;
+                }
+                break;
+
+            case "right":
+                if (this.x < 400) {
+                    updateMoves();
+                    this.x += 100;
+                }
+                break;
+
+            case "up":
+                if (this.y >= 40) {
+                    updateMoves();
+                    this.y -= moveValue;
+                }
+                break;
+            case "down":
+                if (this.y <= 380) {
+                    updateMoves();
+                    this.y += moveValue;
+                }
+                break;
+        }
+    },
+    collision: function () {
+        this.x = 200;
+        this.y = 420;
+        if (this.life > 0) {
+            this.life -= 1;
+            updateLifes(this.life)
+        } else {
+            return;
+        }
+    },
+};
+
+let xRandomic = Math.random() * 10;
+let yRandomic = Math.random() * 10;
+class Gem {
+    constructor(sprite, x, y) {
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), xRandomic + this.x, yRandomic + this.y);
+    }
+}
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+
+let allEnemies = [
+    new Enemy("images/enemy-bug.png", 0, 60),
+    new Enemy("images/enemy-bug.png", 0, 140),
+    new Enemy("images/enemy-bug.png", 0, 220),
+];
+
+let allGems = [
+    new Gem("images/Gem Green.png", Math.random() * 100, Math.random() * 220),
+    new Gem("images/Gem Orange.png", Math.random() * 100, Math.random() * 250),
+    new Gem("images/Gem Blue.png", Math.random() * 100, Math.random() * 200),
+]
+// Place the player object in a variable called player
+
+let player = new Player("images/char-boy.png");
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener("keyup", function (e) {
+    var allowedKeys = {
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down",
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+// Reused from Matching Game Project - Provided by the reviewer
+let gameTimer = el_display => {
+    const game_start_time = new Date().getTime();
+
+    return setInterval(function () {
+        let current_time = new Date().getTime();
+        current_time_played = current_time - game_start_time;
+        let hrs = Math.floor((current_time_played % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((current_time_played % (1000 * 60 * 60 * 24)) / (1000 * 60));
+        let secs = Math.floor((current_time_played % (1000 * 60)) / 1000);
+
+        if (secs < 10) {
+            secs = '0' + secs;
+        }
+
+        el_display.textContent = hrs + ":" + mins + ":" + secs;
+
+        current_time_played = hrs + ":" + mins + ':' + secs;
+
+    }, 500);
+
+}
+
+let endGame = () => {
+
+}
